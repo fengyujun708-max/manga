8485b4c6858415694ef19a6187667d2e45a0a0ce
peace <fengyujun708@gmail.com>
2026-08-17 02:03:14 +0800

    fix: CrashLogDetailScreen 补 import

--- app/src/main/kotlin/com/mangaverse/app/settings/about/crashlog/CrashLogDetailScreen.kt
+++ app/src/main/kotlin/com/mangaverse/app/settings/about/crashlog/CrashLogDetailScreen.kt
@@ -25,6 +25,8 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mangaverse.app.R
+import androidx.compose.ui.res.stringResource
 
 /**
  * Full Compose crash log detail screen replacing:
